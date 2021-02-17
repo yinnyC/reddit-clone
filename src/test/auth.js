5969471e01cbcf8255779df4d10c43bb72039783
Yin <yinnnn17@gmail.com>
2021-02-17 10:12:22 -0800

    Implemented remaining authentication tests

--- src/test/auth.js
+++ src/test/auth.js
@@ -31,6 +31,25 @@ describe("User", function() {
     agent.post("/login", { email: "wrong@wrong.com", password: "nope" }).end(function(err, res) {
       res.status.should.be.equal(401);
       done();
-    });
-  });
+    })
+  })
+  // login
+  it("should be able to login", function(done) {
+    agent
+      .post("/login")
+      .send({ username: "testone", password: "password" })
+      .end(function(err, res) {
+        res.should.have.status(200);
+        agent.should.have.cookie("nToken");
+        done()
+      })
+  })
+  // logout
+  it("should be able to logout", function(done) {
+    agent.get("/logout").end(function(err, res) {
+      res.should.have.status(200);
+      agent.should.not.have.cookie("nToken");
+      done()
+    })
+  })
 });
