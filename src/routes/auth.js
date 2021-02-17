c629587f1e009d7dab321ae1cc92dc92a7146f54
Yin <yinnnn17@gmail.com>
2021-02-16 23:48:57 -0800

    Users can log in

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -6,4 +6,6 @@ const authController = require('../controllers/auth')
 router.get('/sign-up',authController.signupForm)
 router.post('/sign-up',authController.signupUser)
 router.get('/logout',authController.logout)
+router.get('/login',authController.loginForm)
+router.post('/login',authController.login)
 module.exports = router
